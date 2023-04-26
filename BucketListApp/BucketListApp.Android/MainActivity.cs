f4d2699b5a52a00cfdc7d62f23a7732d834b4182
Айматова Шахзода <92733481+bloblobloblob@users.noreply.github.com>
2023-04-26 10:08:28 +0500

    Update MainActivity.cs

--- BucketListApp/BucketListApp.Android/MainActivity.cs
+++ BucketListApp/BucketListApp.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace BucketListApp.Droid
 {
-    [Activity(Label = "BucketListApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "BucketListApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
