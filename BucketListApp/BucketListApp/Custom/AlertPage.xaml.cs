a64083905a8f0f653ca70a3437347f644f8dd76d
ICFA <87987489+ICFA@users.noreply.github.com>
2023-05-07 12:13:58 +0500

    Add AlertPage

---
+++ BucketListApp/BucketListApp/Custom/AlertPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BucketListApp.Custom
+// Пример использования этой страницы
+    //var ap = new AlertPage();
+    //ap.ErrTxt.Text = "Название ошибки";
+    //ap.Txt.Text = "Текст ошибки";
+    //ap.Button1.Text = "Кнопка";
+    //await Navigation.PushModalAsync(ap);
+    //return;
+// Строка выше - опциональна
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlertPage : ContentPage
+    {
+        public Label ErrTxt
+        {
+            get => Up;
+        }
+
+        public Label Txt
+        {
+            get => Med;
+        }
+
+        public Button Button1
+        {
+            get => btn1;
+        }
+
+        public async void Pop(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        public AlertPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- BucketListApp/BucketListApp/Page/CreateGoalPage.xaml.cs
+++ BucketListApp/BucketListApp/Page/CreateGoalPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BucketListApp.Class;
+using BucketListApp.Custom;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -20,16 +21,27 @@ namespace BucketListApp
 
         public async void NewGoal(object sender, EventArgs e)
         {
-            string title = Name.Text.Trim();
-            string cat = Cat.Text.Trim();
-            string desc = About.Text.Trim();
-            string pod = Pod.Text.Trim();
+            await Navigation.PushModalAsync(new AlertPage());
+            return;
+            //string title = Name.Text.Trim();
+            //string cat = Cat.Text.Trim();
+            //string desc = About.Text.Trim();
+            //string pod = Pod.Text.Trim();
 
-            if (string.IsNullOrEmpty(title))
-            {
-                await DisplayAlert("Err", "NoTitle", "Ok");
-                return;
-            }
+            //if (string.IsNullOrEmpty(title))
+            //{
+            //    var ap = new AlertPage();
+            //    ap.LblTitle.Text = "Instructions";
+            //    ap.LblText.Text = "The text to display!";
+            //    ap.Button1.Text = "Done";
+            //    ap.Button1.Clicked += async (s, a) =>
+            //    {
+            //        await Navigation.PopModalAsync();
+            //    };
+            //    ap.Button2.IsVisible = false;
+            //    await Navigation.PushModalAsync(ap);
+            //    return;
+            //}
 
             //Goal goal = new Goal
             //{
